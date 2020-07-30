434ad4202c6a538fa1013881aa3a77e40a593c85
IzayoiRin <1204149237@qq.com>
2020-07-30 14:44:26 +0800

    apt in installer

--- scripts/BSS/goBSS.sh
+++ scripts/BSS/goBSS.sh
@@ -1,10 +1,19 @@
 CHAROOT="${LFS}/chroot"
+INSTALLER="installer.sh"
 
 
 main(){
-	cp -r */ $CHAROOT
-	bash ./pre_basic_sys.sh
+    if [ ! -d $CHAROOT ];then
+        sudo mkdir -v $CHAROOT
+    fi
+    for i in `ls -F | grep "/"` 
+    do
+        echo "copy: ${i} ---> ${CHAROOT}"
+    done
+    sudo cp -r */ $CHAROOT
+    sudo cp -v $INSTALLER $CHAROOT
+    bash pre_basic_sys.sh $*
 }
 
 
-main
+main $*
