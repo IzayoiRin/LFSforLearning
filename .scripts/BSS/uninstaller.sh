d23509208e0c6c7b00d576f0276866ddd3a72f44
IzayoiRin <1204149237@qq.com>
2020-08-12 11:58:22 +0800

    bss checked2

--- .scripts/BSS/uninstaller.sh
+++ .scripts/BSS/uninstaller.sh
@@ -15,22 +15,6 @@ __strip__(){
     rm -rf /tmp/*
 }
 
-__unstall__(){
-	echo "! Remove Temp Toolchains."
-	rm -rf /tools
-
-	echo "! Remove unsuppressed statci libs."
-	rm -vf /usr/lib/lib{bfd,opcodes}.a
-	rm -vf /usr/lib/libbz2.a
-	rm -vf /usr/lib/lib{com_err,e2p,ext2fs,ss}.a
-	rm -vf /usr/lib/libltdl.a
-	rm -vf /usr/lib/libfl.a
-	rm -vf /usr/lib/libz.a
-
-	echo "! Remove necessary files."
-	find /usr/lib /usr/libexec -name \*.la -delete
-}
-
 
 main(){
 	echo "################# STRIP2  #####################"
